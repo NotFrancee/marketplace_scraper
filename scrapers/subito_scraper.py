8baf90d9c90f95bfd5e4cfd5cb62a6d5bded2078
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-07-28 08:24:19 +0200

    modified to make SubitoScraper a child of Scraper

--- scrapers/subito_scraper.py
+++ scrapers/subito_scraper.py
@@ -2,8 +2,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup, Tag
 from classes.product import Product
-
-default_bs_settings = {"features": "lxml"}
+from classes.scraper import Scraper
 
 
 class SubitoProduct(Product):
@@ -48,38 +47,18 @@ class SubitoProduct(Product):
         return description, is_sold
 
 
-class SubitoScraper:
+class SubitoScraper(Scraper):
     """Scraper for subito.it"""
 
-    def __init__(self, query) -> None:
-        self.query = query
+    def __init__(self, query: str) -> None:
+        url_template = "https://www.subito.it/annunci-italia/vendita/fotografia/?q={}"
+        super().__init__(url_template, query, SubitoProduct)
 
         self.products = dict()
 
-    def should_skip(self, product: SubitoProduct):
-        """Checks if the product has already been scraped and skips it in that case"""
-
-        is_already_scraped = product.link in SubitoProduct.products_scraped
-
-        if is_already_scraped:
-            last_update = product.last_updated
-
-            days_since_last_update = pd.Timestamp.today() - last_update
-
-            if days_since_last_update.days < 5:
-                return True
-
-        return False
-
-    def scrape_listings(self):
-        """Scrapes all listings from the last page of the query (TODO not just the last page)"""
-        url_template = "https://www.subito.it/annunci-italia/vendita/fotografia/?q={}"
-        url = url_template.format(self.query.replace(" ", "+"))
-
-        res = requests.get(url, timeout=10)
-        content = BeautifulSoup(res.content, **default_bs_settings)
-
+    def process_listings_page(self, content: BeautifulSoup):
         containers: list[Tag] = content.find_all(attrs={"class": "item-card"})
+        products = dict()
 
         for container in containers:
             name = container.find("h2").text
@@ -87,12 +66,6 @@ class SubitoScraper:
             link = container.find("a").attrs["href"]
 
             product = SubitoProduct(name, price, link)
+            products[link] = product
 
-            if self.should_skip(product):
-                print("product has already been scraped")
-                print(product)
-                continue
-
-            product.scrape()
-
-            self.products[link] = product
+        return products
