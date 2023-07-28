47b90f19dd2f3e2c1c0a3807c7d73e2555d88dcd
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-07-28 08:23:19 +0200

    scraped products are now stored by the scraper

--- classes/product.py
+++ classes/product.py
@@ -4,9 +4,6 @@ from datetime import datetime
 class Product:
     """Base Product Class"""
 
-    products_scraped = set()
-    products = set()
-
     to_row_cols = {
         "name": "Name",
         "price": "Price",
@@ -15,11 +12,6 @@ class Product:
         "last_updated": "Last Update",
         "website": "Website",
     }
-
-    @staticmethod
-    def reset_scraped_products_history():
-        """Resets the history of products scraped"""
-        Product.products_scraped = set()
 
     def __init__(self, name, price, link, website) -> None:
         self.name = name
@@ -31,8 +23,6 @@ class Product:
         self.is_sold = False
         self.last_updated = datetime.now()
         self.website = website
-
-        self.products_scraped.add(link)
 
     def __str__(self) -> str:
         res = []
